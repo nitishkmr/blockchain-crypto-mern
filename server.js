a0e270302b6025db844f1c64211949826773233a
Nitish Kumar <nitishkmr096@gmail.com>
2021-05-17 20:16:34 +0530

    Add api for transacting

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+// Each run server will act like a root and a formation of new blockchain
+// Also, for the most part -> recipient === recipientKey
 const express = require('express');
 const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const wallet = new Wallet();
+const transactionPool = new TransactionPool();
 const pubSub = new PubSub(blockchain);
 app.use(express.json());
 
@@ -26,6 +32,20 @@ app.post('/api/mine', (req, res) => {
   blockchain.addBlock({ data });
   pubSub.broadcastChain(); // to broadcase whenever new block is mined
   res.redirect('/api/blocks');
+});
+
+// @desc To carry forward a transaction
+// @route POST/api/transact
+// @access Public(no token req)
+app.post('/api/transact', (req, res) => {
+  const { amount, recipient } = req.body;
+  const transaction = wallet.createTransaction({
+    recipient,
+    amount,
+  });
+  transactionPool.setTransaction(transaction);
+  console.log('transactionPool', transactionPool);
+  res.json({ transaction });
 });
 
 // to check if there's already an existing chain then this function will be used to sync with it.
