34e30626f3b31fdfb276e6006deb117be80522b7
Nitish Kumar <nitishkmr096@gmail.com>
2021-05-19 00:53:42 +0530

    Setup React as a frontend ( on same backend server )

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const transactionPool = new TransactionPool();
 const pubsub = new PubSub({ blockchain, transactionPool });
 const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
 app.use(express.json());
-app.use(express.static(__dirname + '/client')); // to also send other files like js scripts also to the frontend, or else only the index.html would've been served
+app.use(express.static(__dirname + '/client/dist')); // to also send other files like js scripts also to the frontend, or else only the index.html would've been served
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
@@ -91,7 +91,7 @@ app.get('/api/wallet-info', (req, res) => {
 
 // @desc To serve any endpoints other than the above ones
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/client/index.html');
+  res.sendFile(__dirname + '/client/dist/index.html');
 });
 
 // to check if there's already an existing chain then this function will be used to sync with it.
