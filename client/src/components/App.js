a34eb097daf65cfca2076814dcfdcfe496ee8a80
Nitish Kumar <nitishkmr096@gmail.com>
2021-05-19 02:21:02 +0530

    Seed the backend with initial data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Blocks from './Blocks';
 
 class App extends Component {
   state = { walletInfo: {} }; // will get wallet info from the backend
@@ -17,6 +18,8 @@ class App extends Component {
         <div>Welcome to the Blockchain!</div>
         <div>Address: {address}</div>
         <div>Balance: {balance}</div>
+        <br />
+        <Blocks />
       </div>
     );
   }
